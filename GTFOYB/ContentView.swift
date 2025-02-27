95dd550b4e1d3f5759bc15bdb4efab9c7ab70be5
styvvve <styvvvenguele@icloud.com>
2025-02-27 01:49:08 +0100

    vide

---
+++ GTFOYB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GTFOYB
+//
+//  Created by NGUELE Steve  on 27/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GTFOYB/GTFOYBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTFOYBApp.swift
+//  GTFOYB
+//
+//  Created by NGUELE Steve  on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GTFOYBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
